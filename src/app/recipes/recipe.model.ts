bec6c1089b4d431fdbc0e0876c261714d2348303
torres10luis <Luis.angel_15@yahoo.com>
2018-06-25 11:24:01 -0700

    new recipe service uopdate

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,15 @@
+import { Ingredient } from './../shared/ingredient.model';
+
 export class Recipe {
   public name: string;
   public description: string;
   public imagepath: string;
+  public ingredients: Ingredient [];
 
-  constructor(name: string, description: string, imagePath: string) {
+  constructor(name: string, description: string, imagePath: string, ingredients: Ingredient[]) {
     this.name = name;
     this.description = description;
     this.imagepath = imagePath;
+    this.ingredients = ingredients;
   }
 }
